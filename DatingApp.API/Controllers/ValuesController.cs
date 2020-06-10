7803b8f45fa61b6a614b533af891ccf299c93d1f
Claudio Mondillo <cmondillo@lab.local>
2020-06-10 14:41:07 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -17,8 +17,8 @@ namespace DatingApp.API.Controllers
             _context = context;
 
         } 
-       
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -26,8 +26,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(values);
         }
-
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
